3eb475095bd72759554e1cb9ec4938b6ee1e4920
swiftberries <pthoanganh2009@gmail.com>
2026-06-07 18:31:41 +0700

    Fixing errors

--- src/data/section-order.ts
+++ src/data/section-order.ts
@@ -1,15 +1,15 @@
 export enum Section {
   Education = "education",
   Experience = "experience",
-  // Portfolio = "portfolio",
-  // Publication = "publication",
-  // News = "news",
+  Portfolio = "portfolio",
+  Publication = "publication",
+  News = "news",
 }
 
 export const sectionOrder = [
-  // Section.News,
+  Section.News,
   Section.Education,
-  // Section.Publication,
+  Section.Publication,
   Section.Experience,
-  // Section.Portfolio,
+  Section.Portfolio,
 ];
